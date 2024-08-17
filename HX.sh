8e2d6a82c841ab73ea45c4d23dfc866f59b9ec76
idyll <liushun968858@gmail.com>
2024-08-17 12:04:22 +0800

    Add files via upload
    使用setprop设置系统属性，使其立即生效
    
    Use setprop to set system properties for immediate effect
    
    使用resetprop设置属性，确保重启后仍然有效
    
    Use resetprop to set the properties to ensure that they remain valid after reboot

--- HX.sh
+++ HX.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 [ ! "$MODDIR" ] && MODDIR=${0%/*}
-MODPATH="/data/adb/modules/key"
+MODPATH="/data/adb/modules/hidden"
 date="$( date "+%y年%m月%d日%H时%M分%S秒")"
 [[ ! -e ${MODDIR}/ll/log ]] && mkdir -p ${MODDIR}/ll/log
 function log() {
@@ -11,24 +11,24 @@ function log() {
     fi
 }
 cd "${MODDIR}/ll/log"
-log
 logcat > "${MODDIR}/ll/log/VBMeta_Digest.txt" &
 vbmeta_digest=""
 attempts=("bootlog" "getprop" "logcat" "resetprop" "direct" "fastboot")
 for attempt in "${attempts[@]}"; do
+    log
     case $attempt in
-        "bootlog")
-            vbmeta_digest=$(grep "VBMeta Digest:" "${MODDIR}/ll/log/bootlog.txt" | sed -n 's/.*VBMeta Digest:[[:space:]]*$$.*$$/\1/p')
+        "bootlog")        
+            vbmeta_digest=$(grep "VBMeta Digest:" "${MODDIR}/ll/log/bootlog.txt" | sed -n 's/.*VBMeta Digest:[[:space:]]*$$[^[:space:]]*$$.*/\1/p')
             ;;
         "getprop")
-            vbmeta_digest=$(getprop ro.boot.vbmeta.digest)
+            vbmeta_digest=$(getprop ro.boot.vbmeta.digest 2>/dev/null)
             ;;
         "logcat")
 		    logcat -d | grep "VBMeta Digest:" | awk '{print $3}' > "${MODDIR}/ll/log/VBMeta_Digest.txt"
             vbmeta_digest=$(cat "${MODDIR}/ll/log/VBMeta_Digest.txt")
             ;;
         "resetprop")
-            vbmeta_digest=$(resetprop -n ro.boot.vbmeta.digest)
+            vbmeta_digest=$(resetprop -n ro.boot.vbmeta.digest 2>/dev/null)
             ;;
         "direct")
             if [ -r "/sys/block/bootloader/by-name/vbmeta/digest/sha256" ]; then
@@ -36,11 +36,13 @@ for attempt in "${attempts[@]}"; do
             fi
             ;;
 		"fastboot")
-            vbmeta_digest=$(fastboot oem get vbmeta_digest) 
+            vbmeta_digest=$(fastboot oem get vbmeta_digest 2>/dev/null)
             ;;	
     esac
     if [ -n "$vbmeta_digest" ]; then
         echo "${date}*已找到哈希值：$vbmeta_digest*" >> 哈希值.log
+		su -c setprop ro.boot.vbmeta.digest "$vbmeta_digest"
+		resetprop ro.boot.vbmeta.digest "$vbmeta_digest"
         break
 	else
        echo "${date}*尝试：$attempts获取VBMeta哈希值失败*" >> 哈希值.log
